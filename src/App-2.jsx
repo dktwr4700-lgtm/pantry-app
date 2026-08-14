6f00c87bfe79394cb4a0d9ca850ad8a8dd05d910
dktwr4700-lgtm <dktwr4700@gmail.com>
2026-08-15 01:13:00 +0400

    Update App-2.jsx

--- src/App-2.jsx
+++ src/App-2.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect, useRef } from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
