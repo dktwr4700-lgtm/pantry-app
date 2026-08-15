a7b891c154e9ae21dd785886cdf631088a3c01cb
dktwr4700-lgtm <dktwr4700@gmail.com>
2026-08-15 12:25:29 +0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App-2.jsx";
+import App from "./App-3.jsx";
 
 function showError(msg) {
   const root = document.getElementById("root");
