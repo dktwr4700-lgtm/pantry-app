407b07bb89df71935298e3666a0bc873eb76337e
dktwr4700-lgtm <dktwr4700@gmail.com>
2026-08-16 12:17:36 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App-15.jsx";
+import App from "./App-17.jsx";
 
 function showError(msg) {
   const root = document.getElementById("root");
